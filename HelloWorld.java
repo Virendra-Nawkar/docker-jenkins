ee4d4dba20ca4b2f487d167f0e3abdd07361ed51
Virendra Nawkar <virendranawkar1@gmail.com>
2026-02-25 09:02:55 +0000

    AUto pull

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Virendra! Welcome to DevOps 🚀");
  System.out.println("HI hae made some changes into it");
+System.out.println("Github auto pulling enabled");
     }
 }
